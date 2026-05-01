2d8f6d04aaa9dbe4b72600b644136d69d2cdaada
AkSharma2003 <ankit824012@gmail.com>
2026-05-01 22:05:52 +0530

    code for all operator

---
+++ operator.py
@@ -0,0 +1,47 @@
+#Arithmatic oparator
+print(2+3)
+print(2-3)
+print(2*3)
+print(10/3)
+print(10//3)#integer devision
+print(10%3)
+print(2**3)# 2 to the power 3
+print()
+
+# conditional or relational operator
+print(2>3)
+print(2<3)
+print(2>=3)
+print(2<=3)
+print(2==3)
+print(2!=3)
+print()
+
+#Logical operator
+print(0 and 1)
+print(2 and 5)# return 5
+print(2 or 3)
+print(not 1)
+print()
+
+# bitwise operator
+print(2 & 3)
+print(2 | 3)
+print(2 ^ 3) #xor operator
+print(~3) # not
+print(4 >> 2)# left shift
+print(4 << 2)# right shift
+print()
+
+#Assigment oparator
+a=2
+print(a)
+
+
+#membership operator
+
+print('D'in 'Delhi') # cheak that D is paresent or not in Delhi present then "true"
+print('D' not in 'Delhi') # cheak that D is paresent or not in Delhi not thrn return "true"
+
+
+
