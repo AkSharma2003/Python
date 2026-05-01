a6dabe18eb6055d14849bb3306e650270cc441fd
AkSharma2003 <ankit824012@gmail.com>
2026-05-01 17:43:35 +0530

    basic of python

---
+++ Fundamental.py
@@ -0,0 +1,22 @@
+# x() this is a function 
+#Python is a case sensitve language
+
+print('Helo World')
+print('Helo World',1,2,True,sep='/')
+print('Heloo',end='-')
+print('Ankit')
+print(8)#integer 1e308 handle but not 1e309
+print(1.5) #hadle i.7e308
+#list
+
+print('List',end='-')
+print([1,2,3,4])
+print('Touple',end='-')
+print((1,2,3,45))#touple
+print('Dectionery',end='-')
+print({'Name':'Ankit Kumar Sharma','gender':'Male','weight':45})
+type(3)#it for detect data Type
+a,b,c=1,2,3
+print(a,b,c)
+a=b=c=5
+print(a,b,c);
